4a1a8938692cd2e33a215b194b69670387b8d2fa
Gulab Chopra <c.gulab@gmail.com>
2017-08-23 18:52:24 +0530

    setup mongodb

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
 
 var app = express();
-
 app.get('/', function(req, res) {
 	res.send('This is iWisdom application API');
 });
@@ -10,6 +11,18 @@ app.get('/wisdom', function (req, res){
 	res.send('All wisdom');
 });
 
-app.listen(process.env.PORT, function(){
-	console.log('Listening to port:'+process.env.PORT);
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database){
+	if (err){
+	  console.log(err);
+	  process.exit(1);
+	}
+
+	db = database;
+	console.log("Database connection ready");
+	
+	app.listen(process.env.PORT, function(){
+          console.log('Listening to port:'+process.env.PORT);
+	});
 });

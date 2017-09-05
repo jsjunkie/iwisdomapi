1d7b4dc18c9793b072c79b172fc14ca305bc28f7
Gulab Chopra <c.gulab@gmail.com>
2017-09-05 10:01:51 +0530

    change collection name

--- database.js
+++ database.js
@@ -12,7 +12,7 @@ module.exports.getWisdom = function(db, callback, errorCallback) {
 }
 
 module.exports.addWisdom = function (db, data, callback, errorCallback){
-	var collection = db.collection('iWisdom');
+	var collection = db.collection('Wisdom');
 
 	collection.insert(data, function (err, result) {
 		if (err) {
@@ -25,7 +25,7 @@ module.exports.addWisdom = function (db, data, callback, errorCallback){
 }
 
 module.exports.editWisdom = function (db, data, callback, errorCallback) {
-	var collection = db.collection('iWisdom');
+	var collection = db.collection('Wisdom');
 
 	collection.updateOne({_id: data.key}, { $set: { title: data.title, description: data.description }}, function (err, result) {
 		if (err) {
